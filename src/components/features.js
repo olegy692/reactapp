0f10b5856aaf391f4c622c1d64e86ff2351a2be0
alla2297 <alla2297@edu.easj.dk>
2023-08-21 20:45:25 +0200

    werwr

--- src/components/features.js
+++ src/components/features.js
@@ -1,3 +1,10 @@
+/*!
+* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
 import React from 'react';
 import Upcycle from "./images/Argo_Game_Logo.png";
 import Collect from "./images/recycleBorder.png";
@@ -40,6 +47,13 @@ function Features(){
         padding: '10px',
         
       };*/
+      const elements = [
+        { title: "Opsamle", imgSrc: Collect, alt: "opsamle billedet", description: "Samle affald og få ressourcer til bygge nye genstande" },
+        { title: "Genbrug", imgSrc: Upcycle, alt: "Genbrugs billedet", description: "Samle affald og få ressourcer til bygge nye genstande" },
+        { title: "Byg", imgSrc: Build, alt: "byg billedet", description: "Samle affald og få ressourcer til bygge nye genstande" },
+        { title: "Genanvend", imgSrc: Upcycle, alt: "Genanvend billedet", description: "Samle affald og få ressourcer til bygge nye genstande" },
+        // ...more elements
+      ];
 
     return(
         /*<div class="row ">
@@ -66,28 +80,14 @@ function Features(){
             </div>
             
         </div>*/
-        <div class="row justify-content-center">
-            <div class='col-lg-3 col-md-5 col-sm-12 col-xs-12 bg-primary rounded p-3 border border-dark border-1 mx-2 my-2 mb-2' style={{ maxWidth: '250px', borderRadius: '10%', margin: '0 auto' }}>
-                <h2 >Opsamle</h2>
-                <img class="img-responsive rounded p-3 border border-dark border-1" src={Collect} alt="opsamle billedet" style={{width:'100%',backgroundColor: '#03467A'}}></img>
-                <p>Samle affald og få ressourcer til bygge nye  genstande</p>
+        <div class="row justify-content-center mb-5">
+            {elements.map((element, index) => (
+            <div class='col-lg-3 col-md-5 col-sm-12 col-xs-12 bg-primary rounded p-3 border border-dark border-1 mx-2 my-2 mb-2' key={index} style={{ maxWidth: '250px', borderRadius: '10%', margin: '0 auto' }}>
+                <h2 >{element.title}</h2>
+                <img class="img-responsive rounded p-3 border border-dark border-1" src={element.imgSrc} alt={element.alt} style={{width:'100%',backgroundColor: '#03467A'}}></img>
+                <p>{element.description}</p>
             </div>
-            <div class='col-lg-3 col-md-5 col-sm-12 col-xs-12 bg-primary rounded p-3 border border-dark border-1 mx-2 my-2 mb-2' style={{ maxWidth: '250px', borderRadius: '10%', margin: '0 auto' }}>
-                <h2 >Genbrug</h2>
-                <img class="img-responsive rounded p-3 border border-dark border-1" src={Upcycle} alt="Genbrugs billedet" style={{width:'100%',backgroundColor: '#03467A'}}></img>
-                <p>Samle affald og få ressourcer til bygge nye  genstande</p>
-            </div>
-            <div class='col-lg-3 col-md-5 col-sm-12 col-xs-12 bg-primary rounded p-3 border border-dark border-1 mx-2 my-2 mb-2' style={{ maxWidth: '250px', borderRadius: '10%', margin: '0 auto' }}>
-                <h2 >Byg</h2>
-                <img class="img-responsive rounded p-3 border border-dark border-1" src={Build} alt="byg billedet" style={{width:'100%',backgroundColor: '#03467A'}}></img>
-                <p>Samle affald og få ressourcer til bygge nye  genstande</p>
-            </div>
-            <div class='col-lg-3 col-md-5 col-sm-12 col-xs-12 bg-primary rounded p-3 border border-dark border-1 mx-2 my-2 mb-2' style={{ maxWidth: '250px', borderRadius: '10%', margin: '0 auto' }}>
-                <h2 >Genanvend</h2>
-                <img class="img-responsive rounded p-3 border border-dark border-1" src={Upcycle} alt="Genanvend billedet" style={{width:'100%',backgroundColor: '#03467A'}}></img>
-                <p>Samle affald og få ressourcer til bygge nye  genstande</p>
-            </div>
-            
+            ))}
         </div>
         
     );
