611db67dd370ea18153dfaec3bdd79df57bff2ad
olegy692 <olegsmarcenuks@gmail.com>
2023-08-21 11:05:05 +0200

    GenbrugReact

---
+++ src/components/header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
+import './Header.css';
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <nav className="nav">
+          <ul className="nav-list" style={{ marginLeft: 'auto' }}>
+            <li className="nav-item">
+              <Link to="section1" spy={true} smooth={true} offset={-70} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="section2" spy={true} smooth={true} offset={-70} duration={500}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="section3" spy={true} smooth={true} offset={-70} duration={500}>
+                Features
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="section4" spy={true} smooth={true} offset={-70} duration={500}>
+                Creators
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="logo">Genbrugsspillet</div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
+
+
+
+
+
